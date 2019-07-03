eab16978f813a5551043634c6df100bb5e9460dd
ayumitk <ayumee528@gmail.com>
2019-07-03 06:28:42 -0700

    Add gatsby-plugin-intl

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,164 +1,45 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { Link, graphql } from 'gatsby';
-
+import { Link } from 'gatsby';
+import { injectIntl, FormattedMessage } from 'gatsby-plugin-intl';
 import Layout from '../components/Layout';
-// import Features from '../components/Features'
 import BlogRoll from '../components/BlogRoll';
 
-export const IndexPageTemplate = ({
-  image,
-  title,
-  heading,
-  subheading,
-  // mainpitch,
-  description,
-  // intro,
-}) => (
+
+export const IndexPageTemplate = () => (
   <div>
-    <div
-      className="full-width-image margin-top-0"
-      style={{
-        backgroundImage: `url(${
-          image.childImageSharp ? image.childImageSharp.fluid.src : image
-        })`,
-        backgroundPosition: 'top left',
-        backgroundAttachment: 'fixed',
-      }}
-    >
-      <div>
-        <h2>
-          {title}
-        </h2>
-        <p>
-          {subheading}
-        </p>
-      </div>
-    </div>
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="section">
-          <div className="columns">
-            <div className="column is-10 is-offset-1">
-              <div className="content">
-                {/* <div className="content">
-                  <div className="tile">
-                    <h1 className="title">{mainpitch.title}</h1>
-                  </div>
-                  <div className="tile">
-                    <h3 className="subtitle">{mainpitch.description}</h3>
-                  </div>
-                </div> */}
-                <div className="columns">
-                  <div className="column is-12">
-                    <h3>
-                      {heading}
-                    </h3>
-                    <p>{description}</p>
-                  </div>
-                </div>
-                {/* <Features gridItems={intro.blurbs} /> */}
-                {/* <div className="columns">
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/products">
-                      See all products
-                    </Link>
-                  </div>
-                </div> */}
-                <div className="column is-12">
-                  <h3>
-                    Latest stories
-                  </h3>
-                  <BlogRoll />
-                  <div className="text-center">
-                    <Link className="btn btn-primary" to="/blog">
-                      Read more
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+
+    <section>
+      <div className="container text-center">
+        <h2>Hi, I&apos;m Ayumi</h2>
+        <p>UI/UX Designer & Developer</p>
       </div>
     </section>
+
+    <section className="container">
+      <h3>About Me</h3>
+      <p><FormattedMessage id="about_me.description" /></p>
+      <p><FormattedMessage id="about_me.current" /></p>
+    </section>
+
+    <section className="container">
+      <h3>
+        Latest Tips
+      </h3>
+      <BlogRoll />
+      <div className="text-center">
+        <Link className="btn btn-primary" to="/blog">
+          <FormattedMessage id="read_more" />
+        </Link>
+      </div>
+    </section>
+
   </div>
 );
 
-IndexPageTemplate.propTypes = {
-  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-  title: PropTypes.string,
-  heading: PropTypes.string,
-  subheading: PropTypes.string,
-  mainpitch: PropTypes.object,
-  description: PropTypes.string,
-  intro: PropTypes.shape({
-    blurbs: PropTypes.array,
-  }),
-};
+const IndexPage = () => (
+  <Layout>
+    <IndexPageTemplate />
+  </Layout>
+);
 
-const IndexPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark;
-
-  return (
-    <Layout>
-      <IndexPageTemplate
-        image={frontmatter.image}
-        title={frontmatter.title}
-        heading={frontmatter.heading}
-        subheading={frontmatter.subheading}
-        mainpitch={frontmatter.mainpitch}
-        description={frontmatter.description}
-        intro={frontmatter.intro}
-      />
-    </Layout>
-  );
-};
-
-IndexPage.propTypes = {
-  data: PropTypes.shape({
-    markdownRemark: PropTypes.shape({
-      frontmatter: PropTypes.object,
-    }),
-  }),
-};
-
-export default IndexPage;
-
-export const pageQuery = graphql`
-  query IndexPageTemplate {
-    markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
-      frontmatter {
-        title
-        image {
-          childImageSharp {
-            fluid(maxWidth: 2048, quality: 100) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-        heading
-        subheading
-        mainpitch {
-          title
-          description
-        }
-        description
-        intro {
-          blurbs {
-            image {
-              childImageSharp {
-                fluid(maxWidth: 240, quality: 64) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-            text
-          }
-          heading
-          description
-        }
-      }
-    }
-  }
-`;
+export default injectIntl(IndexPage);
