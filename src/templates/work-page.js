2a1576662ff5633e5bdd7c2d9f5cf270cbcd5a00
ayumitk <ayumee528@gmail.com>
2019-07-04 12:25:36 -0700

    Add behance api

--- src/templates/work-page.js
+++ src/templates/work-page.js
@@ -1,15 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { injectIntl } from 'gatsby-plugin-intl';
 import Layout from '../components/Layout';
 
-export const WorkPageTemplate = () => (
-  <div className="container">
-    <div>
-      <h2>Work</h2>
-      <p>Under Construction</p>
-    </div>
-  </div>
-);
+class WorkPageTemplate extends Component {
+  state = {
+    error: null,
+    isLoaded: false,
+    items: [],
+  };
+
+  componentDidMount() {
+    const apiKey = '5AGtA6uAQot7srZlfWYtj1kZUXSuHx0S';
+    const userID = 'ayumitk';
+    const perPage = 6;
+    // const behanceUserAPI = `https://www.behance.net/v2/users/${userID}/projects?callback=?&api_key=${apiKey}&per_page=${perPage}&callback=callbackUser`;
+    const behanceUserAPI = `https://www.behance.net/v2/users/${userID}/projects?api_key=${apiKey}&per_page=${perPage}`;
+
+    console.log(behanceUserAPI);
+
+    fetch(behanceUserAPI)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.items,
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+        },
+      );
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    console.log(items);
+    return (
+      <div className="container">
+        <h2>Work</h2>
+        <p>Under Construction</p>
+      </div>
+    );
+  }
+}
 
 const WorkPage = () => (
   <Layout>
